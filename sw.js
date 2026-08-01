559c0ccb84ca321acad8c073e879ba001e4b57ea
rulymauzi-byte <rulymauzi@gmail.com>
2026-08-01 11:17:33 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='raumauzi-pro-v7.1.0';
+const CORE=['./','./index.html','./manifest.webmanifest','./logo.png','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  const u=new URL(e.request.url);
+  if(u.hostname.includes('script.google.com'))return;
+  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html'))));
+});
